1cbe90d0405e3393d9a7e69e72981b055d724251
Shashi <shashi.k.parekh@bath.edu>
2021-04-30 11:38:13 +0100

    add router to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Projects from "./pages/Projects"
+import Project from "./pages/Project"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/project/:id">
+          <Project />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
