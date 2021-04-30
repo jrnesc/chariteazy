4fec482653abe3b849cceb4a72f22e9961d308ad
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-30 13:21:22 +0100

    add hero section to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import FeatureCard from "../components/FeatureCard";
 
 const Home = () => {
-  return <FeatureCard />;
+  return (
+    <>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div class="my-auto">
+          <h2 class="text-3xl font-extrabold text-black sm:text-4xl">
+            Charity made eazy.
+          </h2>
+          <p class="text-xl mt-4 text-gray-400">
+            Donate alongside others to multiply your impact. Over one million
+            registered causes.
+          </p>
+        </div>
+        <div>
+          <img src="images/hands.png" class="h-41 mx-auto" />
+        </div>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
+        <FeatureCard />
+        <FeatureCard />
+        <FeatureCard />
+      </div>
+    </>
+  );
 };
 
 export default Home;
