ad3cc9a2c913b41ba02be5964cfe7fab0523e75d
George Goodall <george.goodall@hotmail.co.uk>
2021-05-02 16:04:39 +0100

    update app.js to include route for ProjectsCategory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
+// import About from "./pages/About";
+// import Profile from "./pages/Profile";
 import Projects from "./pages/Projects";
-import Project from "./pages/Project";
+import ProjectsCategory from "./pages/ProjectsCategory";
+// import Project from "./pages/Project";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './index.css';
@@ -18,18 +19,21 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/about">
+            {/* <Route exact path="/about">
               <About />
             </Route>
             <Route exact path="/profile">
               <Profile />
-            </Route>
+            </Route> */}
             <Route exact path="/projects">
               <Projects />
             </Route>
-            <Route exact path="/project/:id">
-              <Project />
+            <Route exact path="/projectscategory">
+              <ProjectsCategory />
             </Route>
+            {/* <Route exact path="/project/:id">
+              <Project />
+            </Route> */}
           </Switch>
         </div>
         <Footer />
