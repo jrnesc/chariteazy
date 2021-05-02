7c058bec9fad3c141288fd21f97e286bd180e9c8
George Goodall <george.goodall@hotmail.co.uk>
2021-05-02 16:01:11 +0100

    add TopProjectCategory component

---
+++ frontend/src/components/TopProjectCategory.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TopProjectCategory = ({title}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            {/* <div>
+                here i want to map through the top 3 projects in a particular category. for example get the top 3 most popular.
+            </div> */}
+        </div>
+    )
+}
+
+export default TopProjectCategory;
