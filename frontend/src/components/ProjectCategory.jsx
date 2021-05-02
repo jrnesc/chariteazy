75dd9112a046c49a74d55f6d2418c339520c8820
George Goodall <george.goodall@hotmail.co.uk>
2021-05-02 16:00:37 +0100

    update ProjectCategory component

--- frontend/src/components/ProjectCategory.jsx
+++ frontend/src/components/ProjectCategory.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-const ProjectCategory = ({title, info}) => {
+const ProjectCategory = () => {
     return (
         <div>
-            <h2>{title}</h2>
-            <h4>{info}</h4>
-            {/* <div>
-                here i want to map through the projects in the particular category.
-            </div> */}
+            Here i want to map through all projects from the selected category  
         </div>
     )
 }
