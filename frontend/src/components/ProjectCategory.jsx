21da3fd73902ddc5e75283956164a83fc2ad9090
George Goodall <george.goodall@hotmail.co.uk>
2021-05-03 17:18:43 +0100

    update ProjectCategory component

---
+++ frontend/src/components/ProjectCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectCategory = ({title}) => {
+    return (
+        <div className="pl-20">
+            <h2 className="text-3xl font-extrabold text-black sm:text-2xl">{title}</h2>
+        </div>  
+    )
+}
+
+export default ProjectCategory;
