b8c8a8167de89247241110b7dbcb992e909c700e
George Goodall <george.goodall@hotmail.co.uk>
2021-05-03 14:02:23 +0100

    add styling to CategoryTitle

--- frontend/src/components/CategoryTitle.jsx
+++ frontend/src/components/CategoryTitle.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const CategoryTitle = ({categoryTitle, info}) => {
+const CategoryTitle = ({categoryTitle, categorySubtitle}) => {
     return (
         <div>
-            <h1>{categoryTitle}</h1>
-            <h4>{info}</h4>
+            <div className="text-center">
+                <h2 className="text-3xl font-extrabold text-black sm:text-4xl">{categoryTitle}</h2>
+                <p className="text-xl mt-4 text-gray-400">{categorySubtitle}</p>
+            </div>
         </div>
     )
 }
 
-export default CategoryTitle
+export default CategoryTitle;
