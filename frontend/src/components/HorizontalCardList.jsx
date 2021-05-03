5bbc71fa5ae7935dc7fc9671e97872977e65851b
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-03 13:27:08 +0100

    add HorizontalCardList component

---
+++ frontend/src/components/HorizontalCardList.jsx
@@ -0,0 +1,60 @@
+import { useRef } from "react";
+import CauseCard from "../components/CauseCard";  // the cards will come as props eventually
+
+const HorizontalCardList = (props) => {
+  const horizontalCardsEl = useRef(null);
+
+  const scroll = (scrollOffset) => {
+    horizontalCardsEl.current.scrollLeft += scrollOffset;
+  };
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <button className="px-5 hidden md:block" onClick={() => scroll(-200)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+        <div
+          className="grid grid-flow-col gap-2.5 auto-cols-max pb-1 overflow-x-scroll"
+          style={{ scrollBehavior: "smooth", scrollbarWidth: "none" }}
+          ref={horizontalCardsEl}
+        >
+          {props.cards.map((card) => (
+            <CauseCard />
+          ))}
+        </div>
+        <button className="px-5 hidden md:block" onClick={() => scroll(200)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default HorizontalCardList;
