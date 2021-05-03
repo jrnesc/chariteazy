1fc398bce9d2bc0676218cad47c85c581b668111
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-03 19:08:25 +0100

    update Dropdown style

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,104 +1,90 @@
-
-import React, { useState, createRef } from 'react';
+import React, { useState, createRef } from "react";
 import { createPopper } from "@popperjs/core";
 
-const Dropdown = ({color}) => {
+const Dropdown = () => {
   const [dropdownPopoverShow, setDropdownPopoverShow] = useState(false);
   const btnDropdownRef = createRef();
   const popoverDropdownRef = createRef();
+  
   const openDropdownPopover = () => {
     createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-      placement: "bottom-start"
+      placement: "bottom-start",
     });
     setDropdownPopoverShow(true);
   };
+
   const closeDropdownPopover = () => {
     setDropdownPopoverShow(false);
   };
 
-  let bgColor;
-  color === "white"
-    ? (bgColor = "bg-blueGray-700")
-    : (bgColor = "bg-" + color + "-500");
   return (
     <>
       <div className="flex justify-end">
-        <div className="w-120 px-4">
-          <div className="relative inline-flex">
-            <button
-              className={
-                "text-3xl font-extrabold text-black sm:text-2xl px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 " +
-                bgColor
-              }
-              type="button"
-              ref={btnDropdownRef}
-              onClick={() => {
-                dropdownPopoverShow
-                  ? closeDropdownPopover()
-                  : openDropdownPopover();
-              }}
-            >
-              <div className="flex items-center">
-                <span>
-                  {color === "white" ? "Select Category" : color + " Dropdown"} 
-                </span>
-                <span>
-                  <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z" />
-                  </svg>
-                </span>
-                
-              </div>
-            </button>
-            <div
-              ref={popoverDropdownRef}
-              className={
-                (dropdownPopoverShow ? "block " : "hidden ") +
-                (color === "white" ? "bg-white " : bgColor + " ") +
-                "text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 "
-              }
-              style={{ minWidth: "12rem" }}
-            >
-              <a
-                href="#category1"
-                className={
-                  "block block px-4 py-2 text-md font-extrabold text-black sm:text-md hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" +
-                  (color === "white" ? " text-blueGray-700" : "text-white")
-                }
-                onClick={e => e.preventDefault()}
+        <div className="relative inline-flex">
+          <button
+            className="relative inline-block text-lg py-2 pl-2 pr-12 rounded border border-black rounded-lg focus:outline-none"
+            type="button"
+            ref={btnDropdownRef}
+            onClick={() => {
+              dropdownPopoverShow
+                ? closeDropdownPopover()
+                : openDropdownPopover();
+            }}
+          >
+            Select category
+            <span className="absolute block h-full w-8 top-0 right-0 rounded-r-lg bg-yellow-300 ml-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute block h-4 w-4 top-1/3 left-1/4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                Category 1
-              </a>
-              <div className="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25" />
-                <a
-                  href="#category2"
-                  className={
-                    "block block px-4 py-2 text-md font-extrabold text-black sm:text-md hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" +
-                    (color === "white" ? " text-blueGray-700" : "text-white")
-                  }
-                  onClick={e => e.preventDefault()}
-                >
-                  Category 2
-                </a>
-              <div className="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25" />
-                <a
-                  href="#category 3"
-                  className={
-                  "block block px-4 py-2 text-md font-extrabold text-black sm:text-md hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" +
-                  (color === "white" ? " text-blueGray-700" : "text-white")
-                  }
-                  onClick={e => e.preventDefault()}
-                >
-                  Category 3
-                </a>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </span>
+          </button>
+          <div
+            ref={popoverDropdownRef}
+            className={
+              (dropdownPopoverShow ? "block " : "hidden ") +
+              "bg-white z-50 py-2 text-left border border-black rounded-lg "
+            }
+            style={{ minWidth: "11.4rem" }}
+          >
+            <a
+              href="#category1"
+              className="block block px-4 py-2 text-md hover:bg-gray-100 hover:text-gray-900"
+              onClick={(e) => e.preventDefault()}
+            >
+              Category 1
+            </a>
+            <div className="h-0 my-2 border border-solid border-t-0 border-black opacity-25" />
+            <a
+              href="#category2"
+              className="block block px-4 py-2 text-md hover:bg-gray-100 hover:text-gray-900"
+              onClick={(e) => e.preventDefault()}
+            >
+              Category 2
+            </a>
+            <div className="h-0 my-2 border border-solid border-t-0 border-black opacity-25" />
+            <a
+              href="#category 3"
+              className="block block px-4 py-2 text-md hover:bg-gray-100 hover:text-gray-900"
+              onClick={(e) => e.preventDefault()}
+            >
+              Category 3
+            </a>
           </div>
         </div>
-      </div>            
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Dropdown;
-
-
