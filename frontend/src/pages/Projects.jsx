5c1bfa20d77c4b6e2db0c7b4c4fddcd8e0c7c2bd
George Goodall <george.goodall@hotmail.co.uk>
2021-05-03 17:20:10 +0100

    update Projects page to inculde all causes, and update spacing styling

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -2,25 +2,46 @@ import React from 'react';
 import CategoryTitle from '../components/CategoryTitle';
 import CauseCard from '../components/CauseCard';
 import Dropdown from '../components/Dropdown';
-import TopProjectCategory from '../components/TopProjectCategory';
+import ProjectCategory from '../components/ProjectCategory';
 
 const Projects = () => {
     return (
         <div>
             <Dropdown color='white' />
-            <CategoryTitle categoryTitle="Categories" categorySubtitle="Please Select a category from the dropdown" />
-            <TopProjectCategory title='most popular' />
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
-               <CauseCard />
-               <CauseCard />
-               <CauseCard /> 
-            </div>
-            <TopProjectCategory title='most recent' />
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
-               <CauseCard />
-               <CauseCard />
-               <CauseCard /> 
-            </div>            
+            <div className="space-y-1">
+                <div className="space-y-20">
+                    <CategoryTitle categoryTitle="Categories" categorySubtitle="Please Select a category from the dropdown" />
+                    <ProjectCategory title='Most Popular' />
+                </div>
+                <div className="space-y-1">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
+                        <CauseCard />
+                        <CauseCard />
+                        <CauseCard /> 
+                    </div>
+                
+                    <ProjectCategory title='Most Recent' />
+                </div>
+                <div className="space-y-1"> 
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
+                        <CauseCard />
+                        <CauseCard />
+                        <CauseCard /> 
+                    </div>
+                    <ProjectCategory title='All Causes' />
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10 gap-y-10">
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />
+                    <CauseCard />  
+                </div>
+            </div>                       
         </div>
     )
 }
