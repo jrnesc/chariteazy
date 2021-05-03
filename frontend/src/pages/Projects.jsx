d757560de12422f0a670aa3993d325b9eb41ab49
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-03 20:09:11 +0100

    fix spacing on Projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -6,38 +6,44 @@ import ProjectCategory from "../components/ProjectCategory";
 const Projects = () => {
   return (
     <div>
-      <Dropdown />
-      <div className="text-center">
+      <div className="text-center py-12">
         <h2 className="text-3xl font-extrabold text-black sm:text-4xl">
           Categories
         </h2>
         <p className="text-xl mt-4 text-gray-400">
-          Please select a category from the dropdown
+          Please select a category from the dropdown.
         </p>
       </div>
-      <ProjectCategory title="Most Popular" />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
+      <Dropdown />
+      <div className="py-12">
+        <ProjectCategory title="Most Popular" />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-12">
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+        </div>
       </div>
-      <ProjectCategory title="Most Recent" />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
+      <div className="py-12">
+        <ProjectCategory title="Most Recent" />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-12">
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+        </div>
       </div>
-      <ProjectCategory title="All Causes" />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10 gap-y-10">
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
-        <CauseCard />
+      <div className="py-12">
+        <ProjectCategory title="All Causes" />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-12">
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+          <CauseCard />
+        </div>
       </div>
     </div>
   );
