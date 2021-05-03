959fd7aa21e62d5ff96d4075bea10d8612f01d16
George Goodall <george.goodall@hotmail.co.uk>
2021-05-03 15:26:19 +0100

    add styling ProjectsCategoryPage

--- frontend/src/pages/ProjectsCategory.jsx
+++ frontend/src/pages/ProjectsCategory.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Dropdown from '../components/Dropdown';
 import CategoryTitle from '../components/CategoryTitle';
-import ProjectCategory from '../components/ProjectCategory';
+import CauseCard from '../components/CauseCard';
 
 
 const ProjectsCategory = () => {
     return (
         <div>
-            <Dropdown color='white' />
-            <CategoryTitle categoryTitle='what is selected from the dropdown' info='some info on charities selected from dropdown' />
-            <ProjectCategory />
+            <Dropdown color="white" />
+            <div className="space-y-10">
+                <CategoryTitle categoryTitle="Category selected from the dropdown" categorySubtitle="some info on category selected from dropdown" />
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10 gap-y-10">
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                <CauseCard />
+                </div>
+            </div>
         </div>
     )
 }
