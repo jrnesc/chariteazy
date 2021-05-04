5d344f36f2c79bcf8ccfaff7f379dbb42f7ec9c1
George Goodall <george.goodall@hotmail.co.uk>
2021-05-04 12:34:22 +0100

    add vote_count to causes models.py

--- backend/causes/models.py
+++ backend/causes/models.py
@@ -7,6 +7,7 @@ class Cause(models.Model):
     founder = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.TextField()
+    vote_count = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
