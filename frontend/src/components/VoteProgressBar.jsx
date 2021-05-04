5a552a1c1c109993ae2b967f258391e17e0fcc91
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-04 10:41:06 +0100

    update VoteProgressBar styling

--- frontend/src/components/VoteProgressBar.jsx
+++ frontend/src/components/VoteProgressBar.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const VoteProgressBar = (props) => {
-    const { completed } = props;
+  const { completed } = props;
 
-    const insideBarStyle = {
-        width: `${completed < 30 ? 30 : completed}%`,
-    }
+  const insideBarStyle = {
+    width: `${completed < 30 ? 30 : completed}%`,
+  };
 
-    return (
-        <div className="flex items-center bg-yellow-400 rounded-full m-5 mx-auto w-4/5">
-            <div
-                className="h-full bg-indigo-600 rounded-full text-right py-5 px-7 "
-                style={insideBarStyle}
-            >
-                <span className="text-white font-bold">
-                    {`Vote Progress ${completed}%`}
-                </span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className="flex overflow-hidden h-10 mb-4 border border-black bg-white rounded-lg"
+      style={{ boxShadow: "0 5px 0px 0px black" }}
+    >
+      <div
+        className="flex flex-col h-full justify-center rounded-r-lg border-r border-black bg-yello text-center "
+        style={insideBarStyle}
+      >
+        <span className="text-white font-bold">
+          {`Vote Progress ${completed}%`}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default VoteProgressBar
+export default VoteProgressBar;
