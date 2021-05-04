9782b8b57ec0404af6b2d7bb94cd6b8f6de7bab7
George Goodall <george.goodall@hotmail.co.uk>
2021-05-04 08:13:16 +0100

    add Cause model to causes models.py

--- backend/causes/models.py
+++ backend/causes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cause(models.Model):
+
+    founder = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
