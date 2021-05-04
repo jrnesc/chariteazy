e0cd7019db528b282d369d93350677ecd6adf187
George Goodall <george.goodall@hotmail.co.uk>
2021-05-04 11:46:15 +0100

    add description and created_at to database

--- backend/causes/models.py
+++ backend/causes/models.py
@@ -6,6 +6,8 @@ class Cause(models.Model):
 
     founder = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
