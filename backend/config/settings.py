bc5e9c5ffcb19564b87f618c26378098adc3123a
George Goodall <george.goodall@hotmail.co.uk>
2021-05-04 08:01:15 +0100

    add django rest framework to config settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',
+
     # local
     'causes',
 ]
@@ -126,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
