a8307227fabaf519389510389c9a755c26ab8b0c
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-05 15:56:31 +0100

    update Profile grid layout

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,23 +1,18 @@
 import UserCard from "../components/UserCard";
 import SavedProjects from "../components/SavedProjects";
-import PaymentOptions from "../components/PaymentOptions";
+import PaymentOptionsForm from "../components/PaymentOptionsForm";
 import ImpactReport from "../components/ImpactReport";
 
 const Profile = () => {
   return (
-    <div className="grid grid-flow-row grid-cols-2 gap-4">
-      <div className="">
+    <div className="-mt-5 md:mt-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <UserCard />
-      </div>
-      <div className="">
-        <SavedProjects />
-      </div>
-      
-      <div className="">
-        <PaymentOptions />
-      </div>
-      <div>
+        <div className="row-span-3">
+         <SavedProjects />
+        </div>
         <ImpactReport />
+        <PaymentOptionsForm />
       </div>
     </div>
   );
