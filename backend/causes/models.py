3c774e328ec2dbfe6cfed89cb1692e238625ed27
George Goodall <george.goodall@hotmail.co.uk>
2021-05-05 09:22:27 +0100

    add ImageField and description field in cause model in Causes app

--- backend/causes/models.py
+++ backend/causes/models.py
@@ -4,12 +4,14 @@ from django.contrib.auth.models import User
 
 class Cause(models.Model):
 
-    founder = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     about = models.TextField()
-    what_we_do = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(
+        default="cause_pics/hands-love.png", upload_to="cause_pics")
     vote_count = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
