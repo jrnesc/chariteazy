a18241a6202270e10f4602ef7417055734c37bbe
George Goodall <george.goodall@hotmail.co.uk>
2021-05-05 10:53:02 +0100

    update users field in Cause model

--- backend/causes/models.py
+++ backend/causes/models.py
@@ -10,7 +10,7 @@ class Cause(models.Model):
     image = models.ImageField(
         default="cause_pics/hands-love.png", upload_to="cause_pics")
     vote_count = models.PositiveIntegerField(default=0)
-    # users = models.ManyToManyField(CustomUser)
+    # users = models.ManyToManyField(CustomUser, on_delete=models.cascade)
     created = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(blank=True, null=True)
 
