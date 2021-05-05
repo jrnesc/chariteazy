806fddfa327ef256ac42d419b20cb4d40495b17d
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-05 20:13:33 +0100

    clean up users models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-# from causes.models import Cause
 from causes.models import Cause
 
 
@@ -9,23 +7,14 @@ from causes.models import Cause
 class CustomUser(AbstractUser):
     # donationAmount = models.OneToOneField()
     # paymentMethod = models.OneToOneField()
-    # favourites = models.ForeignKey(
-    #     Cause, related_name="favourite", default=None, blank=True
-    # )
     def __str__(self):
-        return f"{self.username}"
+        return self.username
 
 
 class Favourite(models.Model):
-    user = models.ForeignKey(
-        CustomUser, related_name="favourites", on_delete=models.CASCADE
-    )
-    
-    cause = models.ForeignKey(
-        Cause, related_name="favourites", on_delete=models.CASCADE
-    )
-    
+    user = models.ForeignKey(CustomUser, related_name="favourites", on_delete=models.CASCADE)
+    cause = models.ForeignKey(Cause, related_name="favourites", on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return f'{self.cause} - {self.user}'
+        return f'{self.user} - {self.cause}'
