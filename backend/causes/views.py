d648192e20ed905ac3a144c6d063baf17aaade65
George Goodall <george.goodall@hotmail.co.uk>
2021-05-05 10:52:12 +0100

    remove IsFounderOrReadOnly permission from causes views.py

--- backend/causes/views.py
+++ backend/causes/views.py
@@ -1,11 +1,9 @@
 from rest_framework import viewsets
 from .models import Cause
-from .permissions import IsFounderOrReadOnly
 from .serializers import CauseSerializer
 
 
 class CauseViewSet(viewsets.ModelViewSet):
 
-    permission_classes = (IsFounderOrReadOnly,)
     queryset = Cause.objects.all()
     serializer_class = CauseSerializer
