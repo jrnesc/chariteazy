4fa45bfa363740937b545c0462128f9f64f6c876
George Goodall <george.goodall@hotmail.co.uk>
2021-05-05 09:23:31 +0100

    add description and image in serializer in Causes app

--- backend/causes/serializers.py
+++ backend/causes/serializers.py
@@ -5,6 +5,6 @@ from .models import Cause
 class CauseSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = ('id', 'founder', 'title', 'about', 'what_we_do',
-                  'vote_count', 'created_at')
+        fields = ('id', 'title', 'about', 'description', 'image',
+                  'vote_count', 'created', 'end_date')
         model = Cause
