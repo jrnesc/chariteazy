3bd37ebeca898553c3529cb7b494251480e3b987
George Goodall <george.goodall@hotmail.co.uk>
2021-05-05 09:22:01 +0100

    update urls.py to take image path

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,7 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
 
-
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
