b3af9b9d373d2def91a35f48faeb2ec7aaad2748
Shashi <shashi.k.parekh@bath.edu>
2021-05-06 09:38:47 +0100

    add post and get requests user favourites

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 
 # import models
 from.models import CustomUser, Favourite
+from causes.models import Cause
 
 # import serializers
 from .serializers import CustomUserSerializer,FavouriteSerializer
@@ -9,6 +10,18 @@ from .serializers import CustomUserSerializer,FavouriteSerializer
 class FavouriteViewSet(viewsets.ModelViewSet):
     queryset = Favourite.objects.all()
     serializer_class = FavouriteSerializer
+    
+    def get_queryset(self):
+        favourites = Favourite.objects.filter(user=self.kwargs["user_pk"])
+        return favourites
+
+    def perform_create(self, serializer):
+        cause = serializer.validated_data['cause']
+        print(serializer.validated_data['cause'])
+        serializer.save(
+            user=self.request.user, cause=cause)
+
+    
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
