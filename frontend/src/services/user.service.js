5520b3bd9ab28250dd07eecd476ca002762ccd25
Shashi <shashi.k.parekh@bath.edu>
2021-05-07 10:21:47 +0100

    manually add cookies for authentication

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,3 +1,10 @@
+import Cookies from 'universal-cookie';
+ 
+const cookies = new Cookies();
+// options doesnt work
+// const cookie_options = {sameSite:'lax',
+//                         httpOnly:true}
+
 const baseUrl = "http://127.0.0.1:8000/api/v1";
 
 const login = async (username, password) => {
@@ -9,7 +16,9 @@ const login = async (username, password) => {
 
   const res = await fetch(`http://127.0.0.1:8000/api/v1/login/`, requestOptions);
   const data = await res.json();
-
+  console.log(data)
+  cookies.set('my-app-auth',data.access_token,{sameSite:'lax'}) // cookie_options
+  cookies.set('my-refresh-token',data.refresh_token,{sameSite:'lax'})
   return data;
 };
 
