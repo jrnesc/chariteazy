48158ae76b3c0b887a373ddd23ad73bf4d706975
Shashi <shashi.k.parekh@bath.edu>
2021-05-07 12:28:51 +0100

    add number of donations to profile

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,4 +1,35 @@
+import { useState, useEffect } from "react";
+import getAuthToken from'../helpers.js'
+
 const UserCard = ({user}) => {
+  const [votes, setVotes] = useState([]);
+  
+
+  useEffect(() => {
+    const getVotes = async () => {
+      const votesFromServer = await fetchVotes();
+      setVotes(votesFromServer);
+    };
+
+    getVotes();
+  }, []);
+
+    // fetch votes
+    const fetchVotes = async () => {
+      const user_id = user['pk']
+      // console.log(user_id)
+      const requestOptions = {
+        method: "GET",
+        credentials:'include',
+        headers: { "Content-Type": "application/json", "Authorization":getAuthToken() },
+       
+      };
+      const res = await fetch(`http://127.0.0.1:8000/api/v1/users/${user_id}/votes/`, requestOptions);
+      const data = await res.json();
+      // console.log(data.length)
+      return data;
+    };
+
   return (
     <div className="rounded-lg bg-yello border border-black p-1">
       <div className="flex flex-col items-center">
@@ -17,7 +48,7 @@ const UserCard = ({user}) => {
       <div className="flex justify-center my-4">
         <div className="mr-4 p-3 text-center">
           <span className="text-xl font-bold block">
-            275
+            {votes.length}
           </span>
           <span className="text-sm">Donations</span>
         </div>
