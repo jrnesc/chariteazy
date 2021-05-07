b8c25c24f3fb471732b72c2ad9c9e76a18137e6d
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-07 10:32:31 +0100

    add mostRecent and mostPopular logic to Projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import CauseCard from "../components/CauseCard";
 import Dropdown from "../components/Dropdown";
 import ProjectCategory from "../components/ProjectCategory";
 
 const Projects = (props) => {
   const [selectedCategory, setSelectedCategory] = useState("All Causes");
+
+  const mostRecent = [...props.causes].sort((a, b) => a.start_date - b.start_date);
+  const mostPopular = [...props.causes].sort((a, b) => b.get_vote_count - a.get_vote_count);
 
   const handleDropdownSelect = (category) => {
     setSelectedCategory(category);
@@ -20,14 +24,28 @@ const Projects = (props) => {
           Please select a category from the dropdown.
         </p>
       </div>
-      <Dropdown onSelect={handleDropdownSelect} />
+      <Dropdown selectedCategory={selectedCategory} onSelect={handleDropdownSelect} />
       <div className="py-12">
         <ProjectCategory title={selectedCategory} />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-12">
-          {selectedCategory === "Most Popular" && <div>Most Popular!</div>}
-          {selectedCategory === "Most Popular" && <div>Most Popular!</div>}
-          {selectedCategory === "All Causes" && <div>All Causes!</div>}
-          {/* props.causes.map((cause, index) => <CauseCard cause={cause} />) */}
+          {selectedCategory === "Most Recent" &&
+            mostRecent.map((cause, index) => (
+              <Link to={{ pathname: "/project/" + cause.id }}>
+                <CauseCard key={index} cause={cause} />
+              </Link>
+            ))}
+          {selectedCategory === "Most Popular" &&
+            mostPopular.map((cause, index) => (
+              <Link to={{ pathname: "/project/" + cause.id }}>
+                <CauseCard key={index} cause={cause} />
+              </Link>
+            ))}
+          {selectedCategory === "All Causes" &&
+            props.causes.map((cause, index) => (
+              <Link to={{ pathname: "/project/" + cause.id }}>
+                <CauseCard key={index} cause={cause} />
+              </Link>
+            ))}
         </div>
       </div>
     </div>
