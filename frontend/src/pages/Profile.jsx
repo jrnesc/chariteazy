d60e107b5427365229c5cd842c74740c88b82cb3
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-06-01 19:08:09 +0100

    clean up Profile component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,40 +3,7 @@ import SavedProjects from "../components/SavedProjects";
 import PaymentOptionsForm from "../components/PaymentOptionsForm";
 import ImpactReport from "../components/ImpactReport";
 
-import {useState,useEffect}  from 'react'
-import getAuthToken from'../helpers.js'
-const Profile = ({user}) => {
-  const[userAccount,setUserAccount]= useState([])
-
-  useEffect(() => {
-    const getUserAccount = async () => {
-      const userAccountFromServer = await fetchUserAccount();
-      setUserAccount(userAccountFromServer);
-    };
-    getUserAccount();
-    console.log(userAccount.donation_amount)
-  }, []);
-
-
-    // fetch causes
-    const fetchUserAccount = async () => {
-      const user_id = user['pk']
-      console.log(user_id)
-      const requestOptions = {
-        method: "GET",
-        credentials:'include',
-        headers: { "Content-Type": "application/json", "Authorization":getAuthToken() },
-       
-      };
-      // console.log(user_id)
-      // console.log(getAuthToken())
-      // const token = getAuthToken()
-      const res = await fetch(`http://127.0.0.1:8000/api/v1/users/${user_id}`, requestOptions);
-      const data = await res.json();
-      // console.log(data)
-      return data;
-    };
-
+const Profile = ({ user }) => {
   return (
     <div className="mt-0 md:mt-5">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
@@ -47,7 +14,7 @@ const Profile = ({user}) => {
           <SavedProjects user={user} />
         </div>
         <div>
-          <PaymentOptionsForm userAccount = {userAccount} />
+          <PaymentOptionsForm user={user} />
         </div>
         <div className="col-span-1 lg:col-span-3">
           <ImpactReport />
